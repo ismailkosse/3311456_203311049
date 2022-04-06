fe13ba6f0dd5d444910255321ec6c28dbbba974f
ismailkosssee <ismailkossee@gmail.com>
2022-04-06 20:01:29 +0300

    yeni

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:deneme5/Ana%20sayfalar/main.dart';
+import 'package:deneme5/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
